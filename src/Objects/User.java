4f3acad677d32df22b646a836cdaec07a603a40f
Richard Paterson <richy1623@gmail.com>
2022-05-10 15:46:31 +0200

    added empty classes for the objects

---
+++ src/Objects/User.java
@@ -0,0 +1,16 @@
+package Objects;
+
+public class User {
+    private String username;
+    private String password;
+
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public boolean authenticate(String u, String p){
+        return username.equals(u) && password.equals(p);
+    }
+}
