94c9d59169fe243f7e5e63a73a7d77d01d9ffb2e
Sebastian Oliver <sebtheoliver@gmail.com>
2022-05-12 17:40:41 +0200

    Added messageID to Client class and a few empty methods. Started on both the NetworkMessage and Message classes.

--- src/Client.java
+++ src/Client.java
@@ -1,19 +1,23 @@
 import java.net.*;
 import java.io.*;
- 
 
 public class Client {
     private static final String hostname = "localhost";
     private static final int port = 5000;
+    private static int messageID = 0;
+
     public static void main(String[] args) {
         sendNetworkMessage("ping");
     }
 
-    //Test method to ping the server and receive a ping back
-    //Server will recieve single messages from client, then restart the connection (Call this method again)
-    public static String sendNetworkMessage(String message){
+    // Test method to ping the server and receive a ping back
+    // Server will recieve single messages from client, then restart the connection
+    // (Call this method again)
+    public static String sendNetworkMessage(String message) {
         try (Socket socket = new Socket(hostname, port)) {
- 
+            // Increment the messageID for every server interaction.
+            ++messageID;
+
             OutputStream output = socket.getOutputStream();
             PrintWriter writer = new PrintWriter(output, true);
 
@@ -25,18 +29,28 @@ public class Client {
             String serverResponse = reader.readLine();
 
             System.out.println(serverResponse);
- 
+
             socket.close();
             return serverResponse;
- 
+
         } catch (UnknownHostException ex) {
- 
+
             System.out.println("Server not found: " + ex.getMessage());
- 
+
         } catch (IOException ex) {
- 
+
             System.out.println("I/O error: " + ex.getMessage());
         }
         return "Failed to send message";
+    }
+
+    // Request to create a new user
+    public static void requestNewUser() {
+        NetworkMessage msg = new NetworkMessage();
+    }
+
+    // Request to login
+    public static void requestLogin() {
+
     }
 }
--- src/Objects/NetworkMessage.java
+++ src/Objects/NetworkMessage.java
@@ -1,5 +1,11 @@
 package Objects;
 
 public class NetworkMessage {
-    
+    private int messageID;
+    private Message msg;
+
+    public NetworkMessage(int messageID, Message msg) {
+        this.messageID = messageID;
+        this.msg = msg;
+    }
 }
