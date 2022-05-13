d387fb56116164c63a906c66adb7a5f621d50a90
Ana Dauane <anadauane@MacBook-Pro.local>
2022-05-13 18:47:13 +0200

    created a the group class

--- src/Objects/Group.java
+++ src/Objects/Group.java
@@ -1,5 +1,29 @@
 package Objects;
 
 public class Group {
+    private String name;
+    private int groupId;
+    private User[] usersList;
+    
+    public Group(String name, int groupId, User[] usersList )
+    {   this.name = name;
+        this.groupId = groupId;
+        this.usersList = usersList; 
+
+    }
+
+    public String getName(){
+        return name; 
+    }
+
+    public int getGroupId(){
+        return groupId;
+    }
+
+    public User[] getUsersList(){
+        return usersList;
+    }
+
+
     
 }
