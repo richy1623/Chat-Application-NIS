88437f513220588d2aedc38f1e73770bee1f4c0d
Sebastian Oliver <sebtheoliver@gmail.com>
2022-06-06 10:57:39 +0200

    Think I just removed an empty line lol.

--- src/Objects/NetworkMessages/Encryption.java
+++ src/Objects/NetworkMessages/Encryption.java
@@ -19,7 +19,6 @@ public class Encryption {
 
     }
 
-
     // Generation of 2040 bit key pair using the RSA algorithm
 
     public static KeyPair generate() throws NoSuchAlgorithmException {
@@ -28,10 +27,10 @@ public class Encryption {
         final SecureRandom random = new SecureRandom();
         generator.initialize(KEY_SIZE, random);
         return generator.generateKeyPair();
-        
+
     }
 
-    // Generation of a 128-bit ASE session key 
+    // Generation of a 128-bit ASE session key
 
     public static SecretKey sessionKey() throws NoSuchAlgorithmException {
 
@@ -39,44 +38,37 @@ public class Encryption {
         SecureRandom random = new SecureRandom();
         random.nextBytes(keyBytes);
         return new SecretKeySpec(keyBytes, "AES");
-        
+
     }
 
-    public static Cipher getAESCipher(SecretKey key) 
-        throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException{
+    public static Cipher getAESCipher(SecretKey key)
+            throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
         Cipher aesCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         aesCipher.init(Cipher.ENCRYPT_MODE, key);
         return aesCipher;
     }
 
-
     // Asymmetric encryption algorithm
 
     public static byte[] encryptionRSA(byte[] messageArray, Key key) throws NoSuchAlgorithmException,
-        NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+            NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
 
-            Cipher cipher = Cipher.getInstance("RSA");
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-            return cipher.doFinal(messageArray);
-        }
-
-
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        return cipher.doFinal(messageArray);
+    }
 
     // Asymmetric decryption algorithm
 
     public static byte[] decryptionRSA(byte[] messageArray, Key key) throws NoSuchAlgorithmException,
-        NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+            NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
 
-            Cipher cipher = Cipher.getInstance("RSA");
-            cipher.init(Cipher.DECRYPT_MODE, key);
-            return cipher.doFinal(messageArray);
-        }
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return cipher.doFinal(messageArray);
+    }
 
-
-
-
-
-    public static String compressZip(String str) throws java.io.UnsupportedEncodingException{
+    public static String compressZip(String str) throws java.io.UnsupportedEncodingException {
         // Encode a String into bytes
         byte[] input = str.getBytes();
 
@@ -89,114 +81,96 @@ public class Encryption {
         compresser.end();
 
         return new String(output, 0, length);
-        
+
     }
 
-    public static String decompressZip(String zip) throws java.io.UnsupportedEncodingException, java.util.zip.DataFormatException{
-        
+    public static String decompressZip(String zip)
+            throws java.io.UnsupportedEncodingException, java.util.zip.DataFormatException {
+
         // Decompress the bytes
         byte[] input = zip.getBytes();
         Inflater decompresser = new Inflater();
         decompresser.setInput(input);
         byte[] result = new byte[100];
         int length = decompresser.inflate(result);
-        
-        //System.out.println(resultLength);
+
+        // System.out.println(resultLength);
         decompresser.end();
         // Decode the bytes into a String
         String outputString = new String(result, 0, length);
         return outputString;
-      
-        
+
     }
 
-    public static void testZipAndConcat() throws java.io.UnsupportedEncodingException, java.util.zip.DataFormatException{
+    public static void testZipAndConcat()
+            throws java.io.UnsupportedEncodingException, java.util.zip.DataFormatException {
         String test = "aaaaa";
         String compress = compressZip(test);
-        System.out.println("compressed: "+compress+"\t"+compress.length());
+        System.out.println("compressed: " + compress + "\t" + compress.length());
         String decompress = decompressZip(compress);
-        System.out.println("decompressed: "+decompress+"\t"+decompress.length());
+        System.out.println("decompressed: " + decompress + "\t" + decompress.length());
         String concat = concatination(decompress, compress);
-        System.out.println("concatination: "+concat);
+        System.out.println("concatination: " + concat);
         String[] deconcat = deconcatination(concat);
-        System.out.println("deconcatination: "+deconcat[0]+"\t"+deconcat[1]);
+        System.out.println("deconcatination: " + deconcat[0] + "\t" + deconcat[1]);
     }
 
     public static String concatination(String message, String hash) {
-        return message+"#%%%#"+hash;
+        return message + "#%%%#" + hash;
     }
 
     public static String[] deconcatination(String message) {
-        return new String[]{message.substring(0, message.indexOf("#%%%#")), message.substring(message.indexOf("#%%%#")+5)};
+        return new String[] { message.substring(0, message.indexOf("#%%%#")),
+                message.substring(message.indexOf("#%%%#") + 5) };
     }
 
     // Symmetric decryption algorithm
     public static byte[] encryptionAES(byte[] messageArray, Key key) throws NoSuchAlgorithmException,
-        NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+            NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
 
-            Cipher cipher = Cipher.getInstance("AES/EBC/PKCS1Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-            return cipher.doFinal(messageArray);
-        }
-
-
+        Cipher cipher = Cipher.getInstance("AES/EBC/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        return cipher.doFinal(messageArray);
+    }
 
     // Symmetric decryption algorithm
 
     public static byte[] decryptionAES(byte[] messageArray, Key key) throws NoSuchAlgorithmException,
-        NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+            NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
 
-            Cipher cipher = Cipher.getInstance("AES/EBC/PKCS1Padding");
-            cipher.init(Cipher.DECRYPT_MODE, key);
-            return cipher.doFinal(messageArray);
-        }
+        Cipher cipher = Cipher.getInstance("AES/EBC/PKCS1Padding");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return cipher.doFinal(messageArray);
+    }
 
+    public static byte[] passEncrypt(byte[] messageArray, String password) throws NoSuchAlgorithmException,
+            NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException,
+            InvalidKeySpecException {
+        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        javax.crypto.spec.PBEKeySpec keySpec = new javax.crypto.spec.PBEKeySpec(password.toCharArray(), messageArray,
+                HASH_ITERATIONS, KEY_SIZE);
+        SecretKeySpec key = new SecretKeySpec(secretKeyFactory.generateSecret(keySpec).getEncoded(), "AES");
 
-        public static byte[] passEncrypt(byte[] messageArray, String password) throws NoSuchAlgorithmException,
-        NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidKeySpecException 
-        {   
-            SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-            javax.crypto.spec.PBEKeySpec keySpec = new javax.crypto.spec.PBEKeySpec( password.toCharArray(), messageArray, HASH_ITERATIONS, KEY_SIZE);
-            SecretKeySpec key = new SecretKeySpec(secretKeyFactory.generateSecret(keySpec).getEncoded(), "AES");
+        Cipher cipher = Cipher.getInstance("AES/EBC/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
 
-            
-            Cipher cipher = Cipher.getInstance("AES/EBC/PKCS1Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-           
-            return cipher.doFinal(messageArray);
+        return cipher.doFinal(messageArray);
 
-        }
-         
+    }
 
-        public static byte[] passcrDecrypt(byte[] messageArray, String password) throws NoSuchAlgorithmException,
-        NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidKeySpecException 
-        {   
-            SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-            javax.crypto.spec.PBEKeySpec keySpec = new javax.crypto.spec.PBEKeySpec( password.toCharArray(), messageArray, HASH_ITERATIONS, KEY_SIZE);
-            SecretKeySpec key = new SecretKeySpec(secretKeyFactory.generateSecret(keySpec).getEncoded(), "AES");
+    public static byte[] passcrDecrypt(byte[] messageArray, String password) throws NoSuchAlgorithmException,
+            NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException,
+            InvalidKeySpecException {
+        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        javax.crypto.spec.PBEKeySpec keySpec = new javax.crypto.spec.PBEKeySpec(password.toCharArray(), messageArray,
+                HASH_ITERATIONS, KEY_SIZE);
+        SecretKeySpec key = new SecretKeySpec(secretKeyFactory.generateSecret(keySpec).getEncoded(), "AES");
 
-            Cipher cipher = Cipher.getInstance("AES/EBC/PKCS1Padding");
-            cipher.init(Cipher.DECRYPT_MODE, key);
-           
-            return cipher.doFinal(messageArray);
+        Cipher cipher = Cipher.getInstance("AES/EBC/PKCS1Padding");
+        cipher.init(Cipher.DECRYPT_MODE, key);
 
-        }
-        
-       
-        
+        return cipher.doFinal(messageArray);
 
-
-
-        
-       
-    
-   
-
-
+    }
 
 }
-
-
-
-
-    
